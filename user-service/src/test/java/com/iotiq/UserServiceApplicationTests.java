ae09f92c3478e1340bf9dde2bf490b3a9aa798ee
Suat Bayır <suatbayir1@gmail.com>
2025-07-07 11:02:20 +0300

    added jenkinsfile

--- user-service/src/test/java/com/iotiq/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/iotiq/UserServiceApplicationTests.java
@@ -1,11 +1,7 @@
 package com.iotiq;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 public class UserServiceApplicationTests {
     @Test
     void contextLoads() {}
