19801e11090c8c020a04a0367362401e7a26081b
Suat Bayır <suatbayir1@gmail.com>
2025-07-09 13:05:40 +0300

    added prometheus, grafana, opentelemetry and jaeger

--- auth-service/src/main/java/com/iotiq/api/controller/AuthController.java
+++ auth-service/src/main/java/com/iotiq/api/controller/AuthController.java
@@ -9,6 +9,8 @@ import com.iotiq.dto.UserDTO;
 import com.iotiq.dto.UserProfileResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,7 @@ import static com.iotiq.message.ResponseMessages.USER_REGISTERED_SUCCESSFULLY;
 @RequiredArgsConstructor
 @RequestMapping(AUTH)
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final RegisterUseCase registerUseCase;
     private final LoginUseCase loginUseCase;
     private final MeUseCase meUseCase;
@@ -29,6 +32,7 @@ public class AuthController {
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ApiResponse<RegisterResponseDTO>> register(@RequestBody @Valid RegisterRequestDTO request) {
         RegisterResponseDTO response = registerUseCase.register(request);
+        logger.info("New user registered: {}", request.getUserName());
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse<>(USER_REGISTERED_SUCCESSFULLY, response));
@@ -37,6 +41,7 @@ public class AuthController {
     @PostMapping(LOGIN)
     public ResponseEntity<ApiResponse<LoginResponseDTO>> login(@RequestBody LoginRequestDTO request) {
         LoginResponseDTO response = loginUseCase.login(request);
+        logger.info("User logged in: {}", request.userName());
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ApiResponse<>(ResponseMessages.LOGIN_SUCCESSFUL.getMessage(), response));
