ce453af63cb641cdff24ee296dbca488651cb267
Suat Bayır <suatbayir1@gmail.com>
2025-07-14 13:32:16 +0300

    configured api-gateway

--- auth-service/src/main/java/com/iotiq/infrastructure/config/RestApis.java
+++ auth-service/src/main/java/com/iotiq/infrastructure/config/RestApis.java
@@ -1,12 +1,10 @@
 package com.iotiq.infrastructure.config;
 
 public class RestApis {
-    public static final String DEVELOPER = "/dev";
-    public static final String TEST = "/test";
-    public static final String RELEASE = "/prod";
+    public static final String API = "/api";
     public static final String VERSION = "/v1";
 
-    public static final String AUTH = DEVELOPER + VERSION + "/auth";
+    public static final String AUTH = API + VERSION + "/auth";
 
     public static final String REGISTER = "/register";
     public static final String LOGIN = "/login";
--- user-service/src/main/java/com/iotiq/infrastructure/config/RestApis.java
+++ user-service/src/main/java/com/iotiq/infrastructure/config/RestApis.java
@@ -1,12 +1,10 @@
 package com.iotiq.infrastructure.config;
 
 public class RestApis {
-    public static final String DEVELOPER = "/dev";
-    public static final String TEST = "/test";
-    public static final String RELEASE = "/prod";
+    public static final String API = "/api";
     public static final String VERSION = "/v1";
 
-    public static final String USER_PROFILE = DEVELOPER + VERSION + "/users";
+    public static final String USER_PROFILE = API + VERSION + "/users";
 
     public static final String CREATE_USER = "/create-user";
     public static final String GET_USER = "/get-user";
